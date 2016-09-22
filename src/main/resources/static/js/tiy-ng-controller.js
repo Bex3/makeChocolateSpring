4ef7c093b57d927e972875de6761e04a706a3ea5
bex3 <Rebeccam.bearden@gmail>
2016-09-22 16:13:50 -0400

    more messed up

--- src/main/resources/static/js/tiy-ng-controller.js
+++ src/main/resources/static/js/tiy-ng-controller.js
@@ -1,10 +1,9 @@
 angular.module('TIYMakeChocolateApp', [])
    .controller('SampleController', function($scope, $http, $timeout) { //Can get these items bc angular injects it for us
 
-        $scope.testValue = "testing here";
-            console.log($scope.testValue);
-
-            console.log("Testing");
+//        $scope.testValue = "testing here";
+//            console.log($scope.testValue);
+//            console.log("Testing");
 
               $scope.makeArray = function(arraySize) {
                     var returnArray = [];
@@ -14,18 +13,30 @@ angular.module('TIYMakeChocolateApp', [])
                     return returnArray;
                 }
 
+        $scope.chocoMaker = function(mcSmalls, mcBigs, mcGoal) {
+            console.log("Trying to make some chocolate"))
 
-            $scope.chocoMaker = function(smalls, bigs, goal) {
-                $http.get("//localhost:8080/makeChocolate.json")
-                    .then(
-                        console.log("Let's make some chocolate");
-                        $scope.chocolateSolution = response.data;
-                        },
-                        function errorCallback(response) {
-                            console.log("Unable to get data");
-                        });
+            $http.get("//localhost:8080/makeChocolate.json?smalls="+ smalls + "&bigs=" + bigs +"&goal=" + goal)
+                .then(
+                    function success(response) {
+                            console.log(response.data);
+                            console.log("made chocolate");
+                            $scope.chocolateSolution = response.data;
 
-            };
+                    },
+                    function errorCallback(response) {
+                           console.log("Unable to get data");
+                    });
+        };
+
+
+                             $scope.makeArray = function(arraySize) {
+                                var returnArray = [];
+                                for (var i = 0; i < arraySize; i++) {
+                                    returnArray.push(i);
+                                }
+                                return returnArray;
+                                }
 
 
     });
