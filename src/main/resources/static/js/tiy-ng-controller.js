cf838a94c28db43732a4cec89bfca2f49fc81771
bex3 <Rebeccam.bearden@gmail>
2016-09-22 12:31:02 -0400

    hardcoded big bars showing

--- src/main/resources/static/js/tiy-ng-controller.js
+++ src/main/resources/static/js/tiy-ng-controller.js
@@ -1,21 +1,17 @@
 angular.module('TIYMakeChocolateApp', [])
    .controller('SampleController', function($scope, $http) {
-       $scope.getGames = function() {
 
-                   console.log("About to go get me some data!");
+    $scope.testValue = "testing here";
+        console.log($scope.testValue);
 
+        console.log("Testing");
 
-                   $http.get("//localhost:8080/makeChocolate.json") //asynch call
-                       .then( //provides the callback
-                           function successCallback(response) { //inside of the promise object then holds the 2 functions
-                               console.log(response.data); //data is the json object as a javascropt object
-                               console.log("Adding data to scope");
-                               $scope.games = response.data;
-                           },
-                           function errorCallback(response) {
-                               console.log("Unable to get data");
-                           });
-                           console.log("Done with the promise - waiting for the callback");
-               };
+              $scope.makeArray = function(arraySize) {
+                    var returnArray = [];
+                    for (var i = 0; i < arraySize; i++) {
+                        returnArray.push(i);
+                    }
+                    return returnArray;
+                }
 
     });
